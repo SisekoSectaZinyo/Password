c4b10792f174ca4c91fdf12a58418363e1ab6092
Emileo Willemse <emileo0301@icloud.com>
2021-04-01 14:31:24 +0200

    my first commit to my milo.java

---
+++ src/main/java/za/ac/cput/Password/milo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.Password;
+
+/**
+ *
+ * @author emileo willemse 219275874
+ */
+public class milo {
+public static void main(String[]args){
+System.out.println("Lets get his project done!");
+}
+}
