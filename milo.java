e7c7000acf0ba1617e2828dc8599ee7c819f8ad8
emileowillemse <80405813+emileowillemse@users.noreply.github.com>
2021-04-01 22:16:03 +0200

    Create milo.java
    This is my source files, milo.java

---
+++ milo.java
@@ -0,0 +1,12 @@
+package za.ac.cput.Password;
+
+/**
+ *
+ * @author emileo willemse 219275874
+ */
+public class milo {
+public static void main(String[]args){
+System.out.println("Welcome to your First Trial on Milo's Company Video Game!");
+System.out.println("Hope you going to enjoy my first demo to an Game!x_x");
+}
+}
